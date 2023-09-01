f0746e5ef497ac548594d58f7d0a2948cd025512
victor <141930128+victorcore@users.noreply.github.com>
2023-09-01 11:43:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const titulo = document.getElementsByTagName
+('h1')
+
+const divis = document.getElementsByClassName('quadrado')
+const redonda = document.getElementsByClassName('circulo')
+
+// querySelector
+// queryselectorAll
+
+const lista = document.querySelectorAll('p')
+
+// console.log(titulo)
+// console.log(divis)
+// console.log(redonda)
+
+console.log(lista)
+
+
+
